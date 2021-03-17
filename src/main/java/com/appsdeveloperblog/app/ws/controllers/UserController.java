2268d37aab305026f047ef74a905dd9357eef258
Illya Sokolov <illya.sokolov82@gmail.com>
2021-03-17 16:42:47 +0200

    Get all users, Pagination

--- src/main/java/com/appsdeveloperblog/app/ws/controllers/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/controllers/UserController.java
@@ -13,6 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -72,6 +75,26 @@ public class UserController {
 		userService.deleteUser(id);
 
 		returnValue.setOperationResult(RequestOperationStatus.SUCCESS.name());
+		return returnValue;
+	}
+
+	@GetMapping(produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	public List<UserRest> getUsers(@RequestParam(value = "page", defaultValue = "0") int page,
+								   @RequestParam(value = "limit", defaultValue = "2") int limit) {
+		List<UserRest> returnValue = new ArrayList<>();
+
+		List<UserDto> users = userService.getUsers(page, limit);
+
+		/*Type listType = new TypeToken<List<UserRest>>() {
+		}.getType();
+		returnValue = new ModelMapper().map(users, listType);*/
+
+		for (UserDto userDto : users) {
+			UserRest userModel = new UserRest();
+			BeanUtils.copyProperties(userDto, userModel);
+			returnValue.add(userModel);
+		}
+
 		return returnValue;
 	}
 
