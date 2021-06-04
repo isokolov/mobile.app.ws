68e0faf4c786f5da6ba7622de8c857391cece7af
Illya Sokolov <illya.sokolov82@gmail.com>
2021-06-04 12:24:02 +0300

    Connect User with Addresses

--- src/main/java/com/appsdeveloperblog/app/ws/controllers/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/controllers/UserController.java
@@ -1,14 +1,16 @@
 package com.appsdeveloperblog.app.ws.controllers;
 
-import com.appsdeveloperblog.app.ws.exceptions.UserServiceException;
+import com.appsdeveloperblog.app.ws.service.AddressService;
 import com.appsdeveloperblog.app.ws.service.UserService;
+import com.appsdeveloperblog.app.ws.shared.dto.AddressDTO;
 import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
 import com.appsdeveloperblog.app.ws.ui.model.request.UserDetailsRequestModel;
-import com.appsdeveloperblog.app.ws.ui.model.response.ErrorMessages;
+import com.appsdeveloperblog.app.ws.ui.model.response.AddressesRest;
 import com.appsdeveloperblog.app.ws.ui.model.response.OperationStatusModel;
 import com.appsdeveloperblog.app.ws.ui.model.response.RequestOperationStatus;
 import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -17,44 +19,42 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @RestController
-@RequestMapping("/users")
+@RequestMapping("users") // http://localhost:8080/users
 public class UserController {
-	
+
 	@Autowired
 	UserService userService;
-	
-	@PostMapping(
-			consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
-			produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
-	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) throws Exception {
-		
-		UserRest returnValue = new UserRest();
 
-		if (userDetails.getFirstName().isEmpty()) {
-			throw new UserServiceException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
-		}
-		/*UserDto userDto = new UserDto();
-		BeanUtils.copyProperties(userDetails, userDto);
-		*/
-        ModelMapper modelMapper = new ModelMapper();
-        UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+	@Autowired
+	AddressService addressService;
 
-		/* UserDto createdUser = userService.createUser(userDto);
-		BeanUtils.copyProperties(createdUser, returnValue); */
-		UserDto createdUser = userService.createUser(userDto);
-		returnValue = modelMapper.map(createdUser, UserRest.class);
 
-		return returnValue;
-	}
 
-	@GetMapping(path = "/{id}",
-            produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+	@GetMapping(path = "/{id}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
 	public UserRest getUser(@PathVariable String id) {
 		UserRest returnValue = new UserRest();
 
 		UserDto userDto = userService.getUserByUserId(id);
-		BeanUtils.copyProperties(userDto, returnValue);
+		ModelMapper modelMapper = new ModelMapper();
+		returnValue = modelMapper.map(userDto, UserRest.class);
+
+		return returnValue;
+	}
+
+	@PostMapping(consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+			MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) throws Exception {
+		UserRest returnValue = new UserRest();
+
+		// UserDto userDto = new UserDto();
+		// BeanUtils.copyProperties(userDetails, userDto);
+		ModelMapper modelMapper = new ModelMapper();
+		UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+
+		UserDto createdUser = userService.createUser(userDto);
+		returnValue = modelMapper.map(createdUser, UserRest.class);
 
 		return returnValue;
 	}
@@ -92,10 +92,6 @@ public class UserController {
 
 		List<UserDto> users = userService.getUsers(page, limit);
 
-		/*Type listType = new TypeToken<List<UserRest>>() {
-		}.getType();
-		returnValue = new ModelMapper().map(users, listType);*/
-
 		for (UserDto userDto : users) {
 			UserRest userModel = new UserRest();
 			BeanUtils.copyProperties(userDto, userModel);
@@ -103,6 +99,34 @@ public class UserController {
 		}
 
 		return returnValue;
+	}
+
+	// http://localhost:8080/mobile-app-ws/users/jfhdjeufhdhdj/addressses
+	@GetMapping(path = "/{id}/addresses", produces = { MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_JSON_VALUE })
+	public List<AddressesRest> getUserAddresses(@PathVariable String id) {
+		List<AddressesRest> returnValue = new ArrayList<>();
+
+		List<AddressDTO> addressesDTO = addressService.getAddresses(id);
+
+		if (addressesDTO != null && !addressesDTO.isEmpty()) {
+			java.lang.reflect.Type listType = new TypeToken<List<AddressesRest>>() {
+			}.getType();
+			returnValue = new ModelMapper().map(addressesDTO, listType);
+		}
+
+		return returnValue;
+	}
+
+	@GetMapping(path = "/{userId}/addresses/{addressId}", produces = { MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE })
+	public AddressesRest getUserAddress(@PathVariable String addressId) {
+
+		AddressDTO addressesDto = addressService.getAddress(addressId);
+
+		ModelMapper modelMapper = new ModelMapper();
+
+		return modelMapper.map(addressesDto, AddressesRest.class);
 	}
 
 }
--- src/main/java/com/appsdeveloperblog/app/ws/security/SecurityConstants.java
+++ src/main/java/com/appsdeveloperblog/app/ws/security/SecurityConstants.java
@@ -12,7 +12,7 @@ import com.appsdeveloperblog.app.ws.SpringApplicationContext;
 * for an existing user */
 public class SecurityConstants {
 
-    public static final long EXPIRATION_TIME = 864000000; // 10 days
+    public static final long EXPIRATION_TIME = 8640000; // 10 days
     public static final long PASSWORD_RESET_EXPIRATION_TIME = 3600000; // 1 hour
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
