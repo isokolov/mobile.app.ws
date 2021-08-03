4b55084c4d33c4e192f2a56d51e41f65b2e146c2
isokolov <illya.sokolov82@gmail.com>
2021-08-03 17:48:49 +0300

    JUnit integration test for Utils with Spring Context

---
+++ src/test/java/com/appsdeveloperblog/app/ws/shared/UtilsTest.java
@@ -0,0 +1,86 @@
+package com.appsdeveloperblog.app.ws.shared;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class UtilsTest {
+
+    @Autowired
+    Utils utils;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    /*
+    public String generateUserId(int length) {
+        return generateRandomString(length);
+    }
+
+    private String generateRandomString(int length) {
+        StringBuilder returnValue = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            returnValue.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
+        }
+
+        return new String(returnValue);
+    } */
+
+    @Test
+    void generateUserId() {
+        String userId = utils.generateUserId(30);
+        String userId2 = utils.generateUserId(30);
+
+        assertNotNull(userId);
+        assertNotNull(userId2);
+
+        assertTrue(userId.length() == 30);
+        assertTrue( !userId.equalsIgnoreCase(userId2) );
+    }
+
+
+    /* public static boolean hasTokenExpired(String token) {
+        boolean returnValue = false;
+
+        try {
+            Claims claims = Jwts.parser().setSigningKey(SecurityConstants.getTokenSecret()).parseClaimsJws(token)
+                    .getBody();
+
+            Date tokenExpirationDate = claims.getExpiration();
+            Date todayDate = new Date();
+
+            returnValue = tokenExpirationDate.before(todayDate);
+        } catch (ExpiredJwtException ex) {
+            returnValue = true;
+        }
+
+        return returnValue;
+    } */
+    @Test
+    final void testHasTokenNotExpired() {
+        String token = utils.generateEmailVerificationToken("TWrOKwvLGpSLk9U4fCcK7AWr5KbEIV");
+        assertNotNull(token);
+
+        boolean hasTokenExpired = Utils.hasTokenExpired(token);
+        assertFalse(hasTokenExpired);
+    }
+
+    @Test
+    final void testHasTokenExpired()
+    {
+        String expiredToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0MUB0ZXN0LmNvbSIsImV4cCI6MTUzMjc3Nzc3NX0.cdudUo3pwZLN9UiTuXiT7itpaQs6BgUPU0yWbNcz56-l1Z0476N3H_qSEHXQI5lUfaK2ePtTWJfROmf0213UJA";
+        boolean hasTokenExpired = Utils.hasTokenExpired(expiredToken);
+
+        assertTrue(hasTokenExpired);
+    }
+}
